dc03b6f4e6f7fa0a6e345b33dd155dbe93c1eeb0
Oscar Hidalgo Alvarez <oscarhidalgo-93@hotmail.com>
2021-07-28 11:50:13 +0200

    Retoque en la aplicacion de Spring para que nos muestre un helloworld en una pagina '/hello'

--- src/main/groovy/com/example/hellosrping/HelloSrpingApplication.groovy
+++ src/main/groovy/com/example/hellosrping/HelloSrpingApplication.groovy
@@ -20,4 +20,6 @@ class HelloSrpingApplication {
         public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
+
 }
