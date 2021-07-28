346f2cf5fc61b1557134fc77f302cd6213a6330b
Oscar Hidalgo Alvarez <oscarhidalgo-93@hotmail.com>
2021-07-28 08:51:42 +0200

    Initial commit

---
+++ src/main/groovy/com/example/hellosrping/HelloSrpingApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.hellosrping
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HelloSrpingApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HelloSrpingApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/example/hellosrping/HelloSrpingApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.hellosrping
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloSrpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
