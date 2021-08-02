ee2615a9b09be41b160583c5cbc7b7f02a962d07
Oscar Hidalgo Alvarez <oscarhidalgo-93@hotmail.com>
2021-08-02 14:25:21 +0200

    Import de assertions core

--- src/test/groovy/com/example/hellosrping/HelloSrpingApplicationTests.groovy
+++ src/test/groovy/com/example/hellosrping/HelloSrpingApplicationTests.groovy
@@ -1,7 +1,10 @@
 package com.example.hellosrping
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import static org.assertj.core.api.Assertions.*
+
 
 @SpringBootTest
 class HelloSrpingApplicationTests {
