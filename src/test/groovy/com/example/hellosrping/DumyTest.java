a00e4bc7b50e66f24b3ade5ac1465b8f82abb4ca
Oscar Hidalgo Alvarez <oscarhidalgo-93@hotmail.com>
2021-08-04 12:01:20 +0200

    Debug test Dummy

--- src/test/groovy/com/example/hellosrping/DumyTest.java
+++ src/test/groovy/com/example/hellosrping/DumyTest.java
@@ -1,12 +1,26 @@
 package com.example.hellosrping;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
-public class DumyTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+ class DumyTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     void Dumy(){
+        assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/spring", String.class))
+                .isEqualTo("Hello MundoSpring");
 
     }
 }
